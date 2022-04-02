261b4705492472a4de6c6927804468951795c999
Hassaan Ahmed <102408829+Hassaan0123@users.noreply.github.com>
2022-04-02 13:42:44 +0500

    Add files via upload

---
+++ Circuit design1.ino
@@ -0,0 +1,53 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(0, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(7, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(0, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(0, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  
+  digitalWrite(2, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(2, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  
+  digitalWrite(4, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(4, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  
+  digitalWrite(5, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(5, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  
+  digitalWrite(7, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(7, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  
+  digitalWrite(5, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(5, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  
+  digitalWrite(4, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(4, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  
+  digitalWrite(2, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(2, LOW);
+  delay(1000); // Wait for 1000 millisecond(s
+}
